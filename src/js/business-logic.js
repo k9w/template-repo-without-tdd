f874790cca937aa0c266bc10b9d8a1b540e592f4
Kevin Williams <kevin@k9w.org>
2022-02-08 13:51:43 -0800

    Add business logic file with example code.

---
+++ src/js/business-logic.js
@@ -0,0 +1,18 @@
+export default class  {
+  static () {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = ;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
