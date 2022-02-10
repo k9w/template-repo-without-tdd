44e7d220a5cc54bdbb122a8cad062ecaa8132772
Kevin Williams <kevin@k9w.org>
2022-02-09 17:02:07 -0800

    Fill more to index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import [constructor] from './business-logic.js'
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+
 $(document).ready(function() {
   $('#dinoaur-form').submit(function(event) {
     event.preventDefault();
